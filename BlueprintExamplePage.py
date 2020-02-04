b97e0b7f9882a22fdb247266d097b1781b154be8
NickLalo <36871227+NickLalo@users.noreply.github.com>
2020-02-03 22:54:14 -0600

    added an error on Blueprint obj name

--- BlueprintExamplePage.py
+++ BlueprintExamplePage.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 neat way to break up our flask app into multiple files so that we don't have one giant file containing every endpoint
 """
 
+# make sure this object is NOT the same name as any function below or you will run into errors when importing
 blueprint_example_page = Blueprint('blueprint_example_page', __name__)
 
 
